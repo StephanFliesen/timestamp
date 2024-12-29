ce6eac35cd0983cf7ae5e39cc9386e6f72492125
Stephan F <Stephan.Fliesen@gmail.com>
2024-12-29 20:55:10 +0000

    finished project

--- index.js
+++ index.js
@@ -14,17 +14,33 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  let timeStamp = req.params.date;
+  if (timeStamp == null) {
+    let date = new Date();
+    return res.json({unix: date.valueOf(), utc: date.toUTCString()});
+  }
 
+  if (/\d{5,}/.test(timeStamp)) {
+    timeStamp = Number(timeStamp);
+  }
+
+  let date = new Date(timeStamp);
+  if (date.toUTCString() == "Invalid Date") {
+    return res.json({error: "Invalid Date"});
+  }
+
+  return res.json({unix: date.valueOf(), utc: date.toUTCString()});
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
